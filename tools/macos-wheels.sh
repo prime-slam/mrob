8b4bb29c8495812a0f41ad3cd93d7c4c0a6bcaf5
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-05-30 06:45:02 +0300

    fix: macos wheel

--- tools/macos-wheels.sh
+++ tools/macos-wheels.sh
@@ -16,8 +16,13 @@ export MACOSX_DEPLOYMENT_TARGET=10.15
 
 cmake -B build 
 
-for PYBIN in /Users/runner/hostedtoolcache/Python/3.*/x64/bin/python*?[0-9]
+# Python 3.6-3.10
+PYTHON_VERSION=("3.6" "3.7" "3.8" "3.9" "3.10")
+
+for v in "${PYTHON_VERSION[@]}"; do
+# for PYBIN in /Users/runner/hostedtoolcache/Python/3.*/x64/bin/python*?[0-9]
 do
+  PYTHON_PATH=(/Users/runner/hostedtoolcache/Python/"${v}"/x64/bin/python*?[0-9])
   "${PYBIN}" -m pip install build
   cmake -B build -DPYTHON_EXECUTABLE="${PYBIN}"
   cmake --build build --target build-wheel
@@ -26,9 +31,3 @@ done
 python3 -m pip install delocate==0.10.4
 
 delocate-wheel -w wheelhouse -v ./build/wheels/*.whl
-
-# for PYBIN in /Users/runner/hostedtoolcache/Python/3.*/x64/bin/python*?[0-9]
-# do
-#   "${PYBIN}" -m pip install deplex --find-links=./build/wheels
-#   "${PYBIN}" ./examples/python/process_cloud.py
-# done
