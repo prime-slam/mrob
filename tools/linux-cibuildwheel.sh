1f693c9ed464cda0ea494fbd10ef2034cf405dc1
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-07-23 00:11:13 +0300

    fix: export arm in script

--- tools/linux-cibuildwheel.sh
+++ tools/linux-cibuildwheel.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 export LC_ALL=C
 
+export CIBW_ARCHS_MACOS="arm64"
+
+
 # Get Python MAJOR.MINOR version to specify Python path for pybind
 PYTHON_VERSION=$(python3 --version | grep -o 3.[0-9]*)
 
