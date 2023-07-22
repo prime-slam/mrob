b6da27d4e55c3aa176869ad89d28d4471124f21a
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-07-23 00:36:20 +0300

    feat: disable manual python find

--- tools/linux-cibuildwheel.sh
+++ tools/linux-cibuildwheel.sh
@@ -7,6 +7,6 @@ export CIBW_ARCHS_MACOS="arm64"
 
 # Get Python MAJOR.MINOR version to specify Python path for pybind
 PYTHON_VERSION=$(python3 --version | grep -o 3.[0-9]*)
-
-cmake -B cppbuild -DPYTHON_EXECUTABLE=$(which python${PYTHON_VERSION})
+#-DPYTHON_EXECUTABLE=$(which python${PYTHON_VERSION})
+cmake -B cppbuild 
 cmake --build cppbuild --target python-package 
