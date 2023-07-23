32139da11c6ce6a4fca978f155f1dd89302ceccc
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-07-23 16:51:22 +0300

    feat: keep manual python find

--- tools/linux-cibuildwheel.sh
+++ tools/linux-cibuildwheel.sh
@@ -7,6 +7,6 @@ export CIBW_ARCHS_MACOS="arm64"
 
 # Get Python MAJOR.MINOR version to specify Python path for pybind
 PYTHON_VERSION=$(python3 --version | grep -o 3.[0-9]*)
-#-DPYTHON_EXECUTABLE=$(which python${PYTHON_VERSION})
-cmake -B cppbuild -DCMAKE_OSX_ARCHITECTURES=arm64
+
+cmake -B cppbuild -DCMAKE_OSX_ARCHITECTURES=arm64 -DPYTHON_EXECUTABLE=$(which python${PYTHON_VERSION})
 cmake --build cppbuild --target python-package 
