43cbbef78cf403bb6b8e1a6b1cc816dda9d5587a
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-07-23 18:53:52 +0300

    feat: test both x86 and arm

--- tools/linux-cibuildwheel.sh
+++ tools/linux-cibuildwheel.sh
@@ -2,11 +2,8 @@
 set -euo pipefail
 export LC_ALL=C
 
-export CIBW_ARCHS_MACOS="arm64"
-
-
 # Get Python MAJOR.MINOR version to specify Python path for pybind
 PYTHON_VERSION=$(python3 --version | grep -o 3.[0-9]*)
 
-cmake -B cppbuild -DCMAKE_OSX_ARCHITECTURES=arm64 -DPYTHON_EXECUTABLE=$(which python${PYTHON_VERSION})
+cmake -B cppbuild -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" -DPYTHON_EXECUTABLE=$(which python${PYTHON_VERSION})
 cmake --build cppbuild --target python-package 
