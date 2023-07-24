6879c103b330bca4ca753e51f1433ea7bfd52e00
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-07-24 13:17:20 +0300

    feat: enable linux build, remove pybind excessive binary artifacts

--- src/pybind/make_python_package.cmake
+++ src/pybind/make_python_package.cmake
@@ -1,3 +1,7 @@
+# Clear DST dir from pybind
+file(GLOB PYBIND_ARTIFACTS ${PYTHON_BINARY_DST}/pybind*)
+file(REMOVE ${PYBIND_ARTIFACTS})
+
 # Copy Python-bindings
 file(INSTALL ${PYTHON_BINARY_DIR}/
-        DESTINATION ${PYTHON_PACKAGE_SRC_DIR}/mrob)
+        DESTINATION ${PYTHON_BINARY_DST}/)
