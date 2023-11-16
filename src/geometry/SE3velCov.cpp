2198d606407c05dc203e02b74666b78b1be0f815
Gonzalo Ferrer <gnz.ferrer@gmail.com>
2023-11-16 13:22:56 +0300

    headers to new classes

--- src/geometry/SE3velCov.cpp
+++ src/geometry/SE3velCov.cpp
@@ -1,3 +1,27 @@
+/* Copyright (c) 2022, Gonzalo Ferrer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * SE3velCov.cpp
+ *
+ *  Created on: June 12, 2021
+ *      Author: Aleksei Panchenko
+ *              Gonzalo Ferrer
+ *              g.ferrer@skoltech.ru
+ *              Mobile Robotics Lab, Skoltech
+ */
+
 #include "mrob/SE3velCov.hpp"
 #include <iostream>
 
