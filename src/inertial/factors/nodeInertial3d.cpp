31d859838f11a79fe13e766c6497dbec12c5fb30
Yisheng Li <yli385@connect.hku.hk>
2024-09-02 16:19:35 +0300

    update node inertial3d

--- src/inertial/factors/nodeInertial3d.cpp
+++ src/inertial/factors/nodeInertial3d.cpp
@@ -30,19 +30,33 @@ NodeInertial3d::NodeInertial3d(const Mat<3,7> state,
                 Node::nodeMode mode):
             Node(15,mode), state_(state), auxiliary_state_(state)
 {
-    //assert
+    //ass000ert
     gyro_bias_ = state.col(5);
     acc_bias_ = state.col(6);
 }
 
 void NodeInertial3d::update(VectRefConst &dx)
 {
-    //Vect<15> dxf = dx;
+    Vect<15> dxf = dx;
+    SE3 T (SO3(state_.block<3, 3>(0, 0)), Mat31(state_.block<3, 1>(0, 3)));
+    T.update_lhs(dxf.head<6>()); 
+    Mat31 V = state_.col(4)+dxf.segment<3>(6); 
+    gyro_bias_ += dxf.segment<3>(9); 
+    acc_bias_ += dxf.tail<3>();  
+    state_<<T.R(), T.t(), V, gyro_bias_, acc_bias_; 
 }
 
 
 void NodeInertial3d::update_from_auxiliary(VectRefConst &dx)
 {
+    Vect<15> dxf = dx;
+    state_ = auxiliary_state_;
+    SE3 T (SO3(state_.block<3, 3>(0, 0)), Mat31(state_.block<3, 1>(0, 3)));
+    T.update_lhs(dxf.head<6>()); 
+    Mat31 V = state_.col(4)+dxf.segment<3>(6); 
+    gyro_bias_ += dxf.segment<3>(9); 
+    acc_bias_ += dxf.tail<3>();  
+    state_<<T.R(), T.t(), V, gyro_bias_, acc_bias_;  
 }
 
 
@@ -55,6 +69,8 @@ void NodeInertial3d::set_state(MatRefConst &x)
 
 void NodeInertial3d::set_auxiliary_state(MatRefConst &x)
 {
+    Mat<3,7> new_state = x;
+    auxiliary_state_ = new_state; 
 }
 
 void NodeInertial3d::print() const
