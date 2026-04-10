1894f9ba8e6c7cf9ca1b884c3266676988c52b33
Aleksei Panchenko <nosmokingsurfer@gmail.com>
2026-04-10 13:07:57 +0300

    [upd]  updating FGraphDiff example

--- python_examples/FGraphDiff_2d.py
+++ python_examples/FGraphDiff_2d.py
@@ -12,8 +12,8 @@ n2 = graph.add_node_pose_2d(x)
 print('node 1 id = ', n1, ' , node 2 id = ', n2)
 
 invCov = np.identity(3)
-graph.add_factor_1pose_2d(np.array([0,0,np.pi/4]),n1,1e6*invCov)
-graph.add_factor_2poses_2d(np.ones(3),n1,n2,invCov)
+graph.add_factor_1pose_2d_diff(np.array([0,0,np.pi/4]),n1,1e6*invCov)
+graph.add_factor_2poses_2d_diff(np.ones(3),n1,n2,invCov)
 
 for i in range(10):
     graph.solve(mrob.FGraphDiff_GN)
